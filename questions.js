b47806d6baa6297b531ff06e33aed023349b90a6
jimmy141319 <jz528198@gmail.com>
2026-03-07 13:24:25 +0800

    Add new questions and options to questions.js

--- questions.js
+++ questions.js
@@ -1,59 +1,74 @@
 const questions = [
-
-{
-subject:"practice",
-q:"面對抱怨較佳做法為何？",
-options:["先傾聽再處理","直接反駁","忽略客人","推給公司"],
-answer:0
-},
-
-{
-subject:"practice",
-q:"導遊最重要的工作為何？",
-options:["確保旅客安全","增加購物","加快行程","安排娛樂"],
-answer:0
-},
-
-{
-subject:"practice",
-q:"導遊服務原則為何？",
-options:["誠信服務","快速結束","購物優先","娛樂優先"],
-answer:0
-},
-
-{
-subject:"law",
-q:"旅客基本權益為何？",
-options:["安全保障","購物優惠","娛樂活動","快速行程"],
-answer:0
-},
-
-{
-subject:"law",
-q:"旅行業服務精神為何？",
-options:["誠信","購物導向","娛樂至上","速度優先"],
-answer:0
-},
-
-{
-subject:"resource",
-q:"阿里山位於哪裡？",
-options:["嘉義","南投","花蓮","台東"],
-answer:0
-},
-
-{
-subject:"resource",
-q:"太魯閣國家公園位於哪裡？",
-options:["花蓮","宜蘭","台東","高雄"],
-answer:0
-},
-
-{
-subject:"resource",
-q:"日月潭位於哪裡？",
-options:["南投","台中","嘉義","花蓮"],
-answer:0
-}
-
+  {
+    subject: "resource",
+    q: "阿里山位於哪個縣？",
+    options: ["嘉義", "南投", "花蓮", "台東"],
+    answer: 0
+  },
+  {
+    subject: "resource",
+    q: "日月潭位於哪個縣？",
+    options: ["南投", "台中", "嘉義", "花蓮"],
+    answer: 0
+  },
+  {
+    subject: "resource",
+    q: "太魯閣國家公園位於哪裡？",
+    options: ["花蓮", "宜蘭", "高雄", "台南"],
+    answer: 0
+  },
+  {
+    subject: "resource",
+    q: "清境農場位於哪裡？",
+    options: ["南投", "台中", "嘉義", "花蓮"],
+    answer: 0
+  },
+  {
+    subject: "law",
+    q: "旅行業服務精神為何？",
+    options: ["誠信", "娛樂至上", "購物優先", "快速結束"],
+    answer: 0
+  },
+  {
+    subject: "law",
+    q: "旅客基本權益之一為何？",
+    options: ["安全保障", "購物優惠", "娛樂活動", "快速行程"],
+    answer: 0
+  },
+  {
+    subject: "law",
+    q: "導遊執業應重視？",
+    options: ["職業倫理", "個人方便", "誇大資訊", "隨意解說"],
+    answer: 0
+  },
+  {
+    subject: "law",
+    q: "旅遊服務核心為何？",
+    options: ["顧客滿意", "大量購物", "快速結束", "減少說明"],
+    answer: 0
+  },
+  {
+    subject: "practice",
+    q: "導遊主要工作為何？",
+    options: ["解說與帶團", "駕駛車輛", "販售商品", "旅館管理"],
+    answer: 0
+  },
+  {
+    subject: "practice",
+    q: "導遊帶團最重要原則？",
+    options: ["安全第一", "購物第一", "速度第一", "娛樂第一"],
+    answer: 0
+  },
+  {
+    subject: "practice",
+    q: "面對抱怨較佳做法為何？",
+    options: ["先傾聽再處理", "直接反駁", "忽略旅客", "推給別人"],
+    answer: 0
+  },
+  {
+    subject: "practice",
+    q: "旅客走失時導遊應先？",
+    options: ["立即尋找並聯絡", "直接離開", "取消行程", "關閉手機"],
+    answer: 0
+  }
 ];
